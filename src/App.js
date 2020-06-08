2bda371e1cb58ea5b9c1dd32e03edca04f2751f6
Pranav Gupta <guptapranav4299@gmail.com>
2020-06-09 00:49:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Todos from './components/Todos';
+import Header from './components/layout/header';
+import AddTodo from './components/AddTodo';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state={
+    todos:[
+      {
+        id:1,
+        title:"Task1",
+        completed:false
+      },
+      {
+        id:2,
+        title:"Task2",
+        completed:false
+      },
+      {
+        id:3,
+        title:"Task3",
+        completed:false
+      }
+    ]
+  }
+
+  // toggle complete
+  markComplete= id =>{
+    this.setState({todos: this.state.todos.map(todo=>{
+      if(todo.id===id)
+      todo.completed=!todo.completed
+
+      return todo;
+    })
+  });    
+  }
+  
+
+// delete todo
+
+
+delTodo=id=>{
+this.setState({todos:[...this.state.todos.filter
+  (todo=>todo.id!==id)]})
+  }
+
+AddTodo=(title)=>{
+  const newTodo={
+    id:4,
+    title,
+    completed:false
+  }
+this.setState({todos:[...this.state.todos,newTodo]}) 
+}
+
+
+  render(){
+    return (
+      <div className="App">
+        
+        <div className="container">
+        <Header></Header>
+        <AddTodo AddTodo={this.AddTodo}></AddTodo>
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}></Todos>
+        </div> 
+      </div>
+      
+    );
+  }
 }
 
 export default App;
