988b94c93fc0417a5f997728868216c167b54b1a
Pranav Gupta <guptapranav4299@gmail.com>
2020-06-09 17:13:04 +0530

    final-commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, {Component} from 'react';
+import  {BrowserRouter as Router, Route} from 'react-router-dom';
 import Todos from './components/Todos';
+import About from './components/pages/About';
 import Header from './components/layout/header';
 import AddTodo from './components/AddTodo';
+// import {v4 as uuidv4}  from 'uuid';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component{
   state={
-    todos:[
-      {
-        id:1,
-        title:"Task1",
-        completed:false
-      },
-      {
-        id:2,
-        title:"Task2",
-        completed:false
-      },
-      {
-        id:3,
-        title:"Task3",
-        completed:false
-      }
-    ]
+    todos:[]
+  }
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(res =>this.setState({todos : res.data})
+    )
   }
 
   // toggle complete
@@ -41,31 +34,38 @@ class App extends Component{
 
 
 delTodo=id=>{
-this.setState({todos:[...this.state.todos.filter
-  (todo=>todo.id!==id)]})
-  }
+
+axios.delete(`https://jsonplaceholder.typicode.com/todos${id}`)
+.then(res=>this.setState({todos:[...this.state.todos.filter
+  (todo=>todo.id!==id)]}))
+}
 
 AddTodo=(title)=>{
-  const newTodo={
-    id:4,
-    title,
-    completed:false
-  }
-this.setState({todos:[...this.state.todos,newTodo]}) 
+  axios.post('https://jsonplaceholder.typicode.com/todos',{title,
+  completed:false}).then(res=>this.setState({todos:[...this.state.todos,res.data]}))
+
 }
 
 
   render(){
     return (
+      <Router>
       <div className="App">
         
         <div className="container">
         <Header></Header>
-        <AddTodo AddTodo={this.AddTodo}></AddTodo>
+        <Route exact path="/" render={props=>(
+          <React.Fragment>
+<AddTodo AddTodo={this.AddTodo}></AddTodo>
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}></Todos>
+          </React.Fragment>
+        )}>
+
+        </Route>
+        <Route path="/about" component={About}></Route>
         </div> 
       </div>
-      
+      </Router>
     );
   }
 }
--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
 function header() {
     return (
         <header style={headerStyle}>
             <h1>TodoList</h1>
+            <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
         </header>
     )
 }
